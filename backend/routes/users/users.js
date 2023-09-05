ce58236574349660840d734f2dae54e3a5963d7e
KoshinHagimoto <koshin.hagimoto@gmail.com>
2023-09-05 17:32:46 +0900

    #9 added signup users

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -2,8 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 const getUser = require("./users-get");
+const crateUser = require("./users-post");
 
 /* GET users listing. */
-router.get("/", getUser);
+router.get("/:id", getUser);
+
+router.post("/signup", crateUser);
 
 module.exports = router;
--- mysql/init/insert_data.sql
+++ mysql/init/insert_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `users` (name, email, password) VALUES("taro", 'hoge@fuga.com', 'pass123');
-INSERT INTO `users` (name) VALUES("jiro");
-INSERT INTO `users` (name) VALUES("tanaka");
+INSERT INTO `User` (userName, email, password) VALUES("taro", 'hoge@fuga.com', 'pass123');
+INSERT INTO `User` (userName) VALUES("jiro");
+INSERT INTO `User` (userName) VALUES("tanaka");
