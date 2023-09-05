dd8c237ff70354c76d0a0cf4054b9893a72e0645
kazukiyoda7 <yodakazu@icloud.com>
2023-09-05 15:06:45 +0900

    #12 add_insert_data

--- mysql/init/insert_data.sql
+++ mysql/init/insert_data.sql
@@ -1,3 +1,9 @@
 INSERT INTO `users` (name, email, password) VALUES("taro", 'hoge@fuga.com', 'pass123');
 INSERT INTO `users` (name) VALUES("jiro");
 INSERT INTO `users` (name) VALUES("tanaka");
+
+INSERT INTO `Food` (userId, foodName, foodImageUrl, expirationDate, quantity, unit, description)
+VALUES
+(1, 'Apple', 'https://example.com/apple.jpg', '2023-09-30', 5.0, 'pieces', 'Fresh and delicious apple'),
+(1, 'Banana', 'https://example.com/banana.jpg', '2023-09-28', 3.0, 'pieces', 'Ripe and sweet banana'),
+(2, 'Carrot', 'https://example.com/carrot.jpg', '2023-09-25', 2.5, 'pieces', 'Organic carrot for sale');
