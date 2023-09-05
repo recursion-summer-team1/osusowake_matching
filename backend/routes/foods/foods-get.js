8f617274b7f3f64e89db5593e958de426437ed13
rasuku0402 <taitoo0402@gmail.com>
2023-09-05 16:05:44 +0900

    #12 foodテーブルへの新規追加のPOSTメソッドを実現するAPIを作成。(画像をどうするかはまだ決めていない)

--- backend/routes/foods/foods-get.js
+++ backend/routes/foods/foods-get.js
@@ -1,14 +1,14 @@
 const pool = require("../../mysqlConnection");
 
 const getFood = (req, res) => {
-	pool.query("SELECT * from Food;", (err, results) => {
-		if (err) {
-			console.error("user.js: sql execute error");
-		} else {
-			console.log("user.js: sql execute success");
-		}
-		res.send(results);
-	});
+  pool.query("SELECT * from Food;", (err, results) => {
+    if (err) {
+      console.error("user.js: sql execute error");
+    } else {
+      console.log("user.js: sql execute success");
+    }
+    res.send(results);
+  });
 };
 
 module.exports = getFood;
--- backend/routes/foods/foods-post.js
+++ backend/routes/foods/foods-post.js
@@ -1,17 +1,41 @@
 const pool = require("../../mysqlConnection");
 
 const postFood = (req, res) => {
-    pool.query("INSERT INTO Food (userId, foodName, foodImageUrl, isSoldout, expirationDate, quantity, unit, description) VALUES (? ? ? ? ? ? ? ?)",
-     [req.body.itemName],
-     (err, results) => {
-      if (err) {
-        console.error("foods.js: sql execute error");
-      } else {
-        console.log("foods.js: sql execute success");
-      }
-      res.send(results);
+  // 必須のフィールドがnullであるかどうかをチェック
+  if (
+    req.body.expirationDate === null ||
+    req.body.quantity === null ||
+    req.body.unit === null ||
+    req.body.description === null
+  ) {
+    return res.status(400).send({
+      message: "Bad Request",
+      error: "expirationDate or quantity cannot be null",
     });
-  };
-  
-  module.exports = postFood;
-  
+  }
+  pool.query(
+    "INSERT INTO Food (userId, foodName, foodImageUrl, isSoldout, expirationDate, quantity, unit, description) VALUES (?,?,?,?,?,?,?,?)",
+    [
+      req.body.userId,
+      req.body.foodName,
+      req.body.foodImageUrl,
+      req.body.isSoldout,
+      req.body.expirationDate,
+      req.body.quantity,
+      req.body.unit,
+      req.body.description,
+    ],
+    (err, results) => {
+      if (err) {
+        console.error("foods.js: sql execute error:", err);
+        return res
+          .status(500)
+          .send({ message: "Internal Server Error", error: err.message });
+      }
+      console.log("foods.js: sql execute success");
+      res.send(results);
+    },
+  );
+};
+
+module.exports = postFood;
