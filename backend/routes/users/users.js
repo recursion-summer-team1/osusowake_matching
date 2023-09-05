58ecd43e5820909d948e864cfd7112da65babf54
KoshinHagimoto <koshin.hagimoto@gmail.com>
2023-09-05 18:15:07 +0900

    #9 added delete User

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 const getUser = require("./users-get");
 const crateUser = require("./users-post");
+const deleteUser = require("./users-delete");
 
-/* GET users listing. */
+/* GET User listing. */
 router.get("/:id", getUser);
-
+/* POST User */
 router.post("/signup", crateUser);
+/* DELETE User */
+router.delete("/:id", deleteUser);
 
 module.exports = router;
