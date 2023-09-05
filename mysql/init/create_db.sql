d5cad5a30f1cec36b9a247564fe906b1e6283999
kazukiyoda7 <yodakazu@icloud.com>
2023-09-05 14:49:27 +0900

    #12 add food table

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -4,11 +4,28 @@ USE `dbname`;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL AUTO_INCREMENT,
   `name` TEXT NOT NULL,
   `email` TEXT,
   `password` TEXT,
   `create_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`userId`)
+);
+
+DROP TABLE IF EXISTS `Food`;
+CREATE TABLE `Food` (
+  `foodId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `foodName` TEXT NOT NULL,
+  `foodImageUrl` TEXT,
+  `isSoldOut` BOOLEAN NOT NULL DEFAULT FALSE,
+  `expirationDate` DATE,
+  `quantity` FLOAT,
+  `unit` TEXT,
+  `description` TEXT,
+  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`foodId`),
+  FOREIGN KEY (`userId`) REFERENCES `users`(`userId`) ON DELETE CASCADE
 );
