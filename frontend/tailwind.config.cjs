0d7b6dfcce803e8e28a4a121477d9c44a17c61a7
toddlerer <74579078+toddlerer@users.noreply.github.com>
2023-09-05 13:54:37 +0900

    fix: daisyui data-theme not reflected

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,4 +4,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          ...require("daisyui/src/theming/themes")["[data-theme=cupcake]"],
+          // "primary": "blue",
+        },
+      },]
+  },
 };
